28c74642e3e72ebe9db8f42af5c9ef34467aab33
DEVita42 <dvt.lorenzo@gmail.com>
2024-05-08 16:15:17 +0200

    Function listTask added

--- taskManagementSystem.js
+++ taskManagementSystem.js
@@ -5,18 +5,18 @@ const taskNames = [];
 // Define a function named addTask that takes two parameters:
 
 async function addTask(task, resultHandler) {
-// Inside the addTask function, use setTimeout to simulate an asynchronous operation.
+  // Inside the addTask function, use setTimeout to simulate an asynchronous operation.
   setTimeout(() => {
     if (!task || typeof task !== 'string') {
       // If the task parameter is empty or not a string, invoke the callback function
       // with an Error object containing the message "Task must be a non-empty string".
-      const error = new Error('Task must be a non-empty string')
+      const error = new Error('Task must be a non-empty string');
       resultHandler(error);
     } else {
       // Otherwise, add the task to the tasks array and invoke the callback function
       // with null as the first parameter and the message "Task added successfully" as the second parameter.
       taskNames.push(task);
-      resultHandler(null, "Task added successfully");
+      resultHandler(null, 'Task added successfully');
     }
   }, 2000);
 }
@@ -40,4 +40,14 @@ async function completeTask(index, callback) {
       callback(null, `Task: ${completedTask} completed successfully.`);
     }
   }, 2000);
+}
+
+// Function to List Tasks:
+// Define a function named listTasks that does not take any parameters.
+
+function listTasks() {
+  // Inside the listTasks function, log each task in the tasks array to the console, along with its corresponding index.
+    taskNames.forEach((task, index)=>{
+      console.log(`task ${index + 1}: ${task}`)
+    })
 }
